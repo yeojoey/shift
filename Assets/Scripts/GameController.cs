b211e99c10e956d8f304f28c2c1c7c160773e1f3
DESKTOP\Joey <yeojoey@gmail.com>
2017-12-09 01:31:11 +0800

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text timeLeftText;
+	public float gameLength = 100f;
+	public float timeLeft;
+
+	void Start () {
+		timeLeft = gameLength;
+	}
+
+	void Update () {
+
+		if (timeLeft > 0) {
+			timeLeft -= Time.deltaTime;
+			timeLeftText.text = ((int) timeLeft).ToString ();
+		} else {
+			// LOSE
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public enum State
+	{
+		Idle,
+		Walking_Right,
+		Walking_Left,
+		Going_Ghost,
+		Going_Real
+
+	}
+
+
+	public State currentState;
+	public float speed = 5f;
+	public bool isGhost;
+	public float transformTime = 0.5f;
+
+	private BoxCollider2D collider;
+
+	void Start ()
+	{
+		isGhost = false;
+		collider = GetComponent<BoxCollider2D> ();
+		currentState = State.Idle;
+	}
+
+	void Update ()
+	{
+
+		transform.Translate (speed * 0.01f * (Vector2.right * Input.GetAxis ("Horizontal") + Vector2.up * Input.GetAxis ("Vertical")));
+
+		switch (currentState) {
+
+		case State.Idle:
+		case State.Walking_Left:
+		case State.Walking_Right:
+			if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.RightShift)) {
+				if (!isGhost) {
+					StartCoroutine (GoGhost ());
+				} else {
+					StartCoroutine (GoReal ());
+				}
+			}
+			break;
+
+		default:
+			break;
+
+		}
+
+	}
+
+	private IEnumerator GoGhost ()
+	{
+		currentState = State.Going_Ghost;
+		float elapsedTime = 0f;
+		while (elapsedTime < transformTime) {
+			elapsedTime += Time.unscaledDeltaTime;
+			yield return null;
+		}
+		isGhost = true;
+		collider.enabled = false;
+		currentState = State.Idle;
+	}
+
+	private IEnumerator GoReal ()
+	{
+		currentState = State.Going_Real;
+		float elapsedTime = 0f;
+		while (elapsedTime < transformTime) {
+			elapsedTime += Time.unscaledDeltaTime;
+			yield return null;
+		}
+		isGhost = false;
+		collider.enabled = true;
+		currentState = State.Idle;
+	}
+
+}
